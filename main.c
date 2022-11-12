7cb6288fd5850cfad6968016e8a0b89b72ea487c
RedaMeziouni <meziounidb@gmail.com>
2022-11-12 11:43:42 +0100

    Create second cube

--- main.c
+++ main.c
@@ -92,6 +92,22 @@ int main()
                 calculateForSurface(cubeX, cubeWidth, cubeY, '+');
             }
         }
+        cubeWidth = 10;
+        horizontalOffset = 1 * cubeWidth;
+        // 2nd CUBE
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
+        {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+                 cubeY += incrementSpeed)
+            {
+                calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+                calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+                calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+                calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+                calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+                calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+            }
+        }
     };
     return 0;
 }
