6adc84877d21d90ac2cd29dc2feb6eaec660287e
RedaMeziouni <meziounidb@gmail.com>
2022-11-12 11:15:31 +0100

    Calculate Y axis function

--- main.c
+++ main.c
@@ -11,6 +11,14 @@ float calculateX(int i, int j, int k)
            j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
 }
 
+// Calculate Y axis
+float calculateY(int i, int j, int k)
+{
+    return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
+           j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
+           i * cos(B) * sin(C);
+}
+
 // Main
 int main()
 {
