a38497c2ac28a3bfaa26f33ad2b84621fb26314d
RedaMeziouni <meziounidb@gmail.com>
2022-11-12 11:39:55 +0100

    Create first cube

--- main.c
+++ main.c
@@ -77,6 +77,21 @@ int main()
         memset(zBuffer, 0, width * height * 4);
         cubeWidth = 20;
         horizontalOffset = -2 * cubeWidth;
+
+        // 1st CUBE
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
+        {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+                 cubeY += incrementSpeed)
+            {
+                calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+                calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+                calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+                calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+                calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+                calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+            }
+        }
     };
     return 0;
 }
