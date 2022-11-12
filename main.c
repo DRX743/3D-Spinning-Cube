578e3672a4814674d1c781ba896af0b6ed503720
RedaMeziouni <meziounidb@gmail.com>
2022-11-12 11:47:57 +0100

    Create third cube

--- main.c
+++ main.c
@@ -94,7 +94,25 @@ int main()
         }
         cubeWidth = 10;
         horizontalOffset = 1 * cubeWidth;
+
         // 2nd CUBE
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
+        {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+                 cubeY += incrementSpeed)
+            {
+                calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+                calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+                calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+                calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+                calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+                calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+            }
+        }
+        cubeWidth = 5;
+        horizontalOffset = 8 * cubeWidth;
+
+        // 3rd CUBE
         for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
         {
             for (float cubeY = -cubeWidth; cubeY < cubeWidth;
