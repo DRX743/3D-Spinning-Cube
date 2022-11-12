2f4dfa6af3a049ac6100b6d5569961ef1b8e05cd
RedaMeziouni <meziounidb@gmail.com>
2022-11-12 11:12:26 +0100

    Include Math Library and create calulate X function

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
+// Based on the formula (Formula.png)
+float A, B, C;
+
+// Calculate the X axis
+float calculateX(int i, int j, int k)
+{
+    return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
+           j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
+}
+
+// Main
+int main()
+{
     printf("Hello World \n");
     return 0;
 }
