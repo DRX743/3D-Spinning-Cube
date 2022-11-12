23d69e65733016e87823233e44766645eb5529a7
RedaMeziouni <meziounidb@gmail.com>
2022-11-12 11:36:06 +0100

    Create calculate surface function

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+#include <unistd.h>
 
 // Global Variables
 float cubeWidth = 20;
@@ -10,6 +12,13 @@ int backgroundASCIICode = '.';
 int distanceFromCam = 100;
 float horizontalOffset;
 float K1 = 40;
+
+float incrementSpeed = 0.6;
+
+float x, y, z;
+float ooz;
+int xp, yp;
+int idx;
 
 // Based on the formula (Formula.png)
 float A, B, C;
@@ -35,10 +44,39 @@ float calculateZ(int i, int j, int k)
     return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
 }
 
+// Calculate surface
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch)
+{
+    x = calculateX(cubeX, cubeY, cubeZ);
+    y = calculateY(cubeX, cubeY, cubeZ);
+    z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
+
+    ooz = 1 / z;
+
+    xp = (int)(width / 2 + horizontalOffset + K1 * ooz * x * 2);
+    yp = (int)(height / 2 + K1 * ooz * y);
+
+    idx = xp + yp * width;
+    if (idx >= 0 && idx < width * height)
+    {
+        if (ooz > zBuffer[idx])
+        {
+            zBuffer[idx] = ooz;
+            buffer[idx] = ch;
+        }
+    }
+}
+
 // Main
 int main()
 {
     printf("\x1b[2J"); //\x1b is the ASCII for ESCAPE (literally the ESC key on your keyboard). [2K is the command "erase the current line"
-    while (1){};
+    while (1)
+    {
+        memset(buffer, backgroundASCIICode, width * height);
+        memset(zBuffer, 0, width * height * 4);
+        cubeWidth = 20;
+        horizontalOffset = -2 * cubeWidth;
+    };
     return 0;
 }
